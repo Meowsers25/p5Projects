d503aa1ee92d2e4674056bda706bf976910fe39b
Chris <c.lamothe25@gmail.com>
2017-10-14 09:50:32 -0400

    finished fireworks code along

--- practice1/fireWorks/public/sketch.js
+++ practice1/fireWorks/public/sketch.js
@@ -1,17 +1,31 @@
-var firework;
+var fireworks = [];
+var gravity;
 
 
 function setup() {
   // put setup code here
   createCanvas(400, 300);
+  colorMode(HSB);
+  gravity = createVector(0, 0.2);
+
   stroke(255);
   strokeWeight(4);
-  firework = new Particle(random(width), height);
+  //firework = new Particle(random(width), height
+  background(0);
 }
 
 function draw() {
-  background(51);
-  firework.update();
-  firework.show();
-
+  colorMode(RGB);
+  background(0, 0, 0, 25);
+  if (random(1) < 0.03) {
+    fireworks.push(new Firework());
+  }
+  for (var i = fireworks.length - 1; i >= 0; i--) {
+    fireworks[i].update();
+    fireworks[i].show();
+    if (fireworks[i].done()) {
+      fireworks.splice(i, 1);
+    }
+  }
+  console.log(fireworks.length);
 }
