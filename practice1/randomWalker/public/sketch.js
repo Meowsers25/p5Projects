446f2d6ed624ac897921fa1d1cd147b4b197bdfc
Chris <c.lamothe25@gmail.com>
2017-10-16 18:07:19 -0400

    added acceleration towards mouse

--- practice1/randomWalker/public/sketch.js
+++ practice1/randomWalker/public/sketch.js
@@ -16,15 +16,24 @@ function draw() {
 function Walker() {
   //create vector-line 18 replaces lines 19, 20
   //must add .pos (vector object) to the x and y positions
-  this.pos = createVector(width/2, 0);
+  this.pos = createVector(width/2, height/2);
   this.vel = createVector(0,0);
-  this.acc = createVector(0, 0.1);
+  this.acc = p5.Vector.fromAngle(random(TWO_PI));
+  this.acc.setMag(0.2);
   //this.x = width/2;
   //this.y = height/2
 
   //change .walk to .update
   this.update = function() {
+    // var mouse = createVector(mouseX, mouseY);
+    // this.acc = p5.Vector.sub(mouse, this.pos);
+    // this.acc.normalize();
+    // this.acc.mult(0.5);
+
+    // this.acc = createVector(random(-1, 1), random(-1, 1));
+    // this.acc.mult(0.1);
     //added vector math; replaces lines 27&28; use .add()
+    this.acc.rotate(0.1);
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     // this.pos.x = this.pos.x + random(-10, 10);
