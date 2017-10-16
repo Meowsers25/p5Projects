d61cf318cb6fcbfd3b8e7c2820348a4cab3e8e9a
Chris <c.lamothe25@gmail.com>
2017-10-16 18:26:52 -0400

    added perlin noise activity

---
+++ practice1/perlinNoise/public/sketch.js
@@ -0,0 +1,20 @@
+var xoff = 0;
+
+
+function setup() {
+  // put setup code here
+  createCanvas(640, 360);
+}
+
+function draw() {
+  background(51);
+
+  //var x = random(0, width);
+  var x = noise(xoff) * width;
+
+  xoff += 0.05;
+
+  fill(255);
+  ellipse(x, 180, 48, 48);
+
+}
